2d2d2c14e9e0ae38e67a8ee498ab1d8ab4e03ac4
Maria Snegova <Marya0@yandex.ru>
2022-04-25 23:14:34 +0300

    feat: подгрузить карточки из массива

--- script/script.js
+++ script/script.js
@@ -1,3 +1,40 @@
+//массив мест
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//шаблоны
+
+const galleryCardTemlate = document
+  .querySelector('#gallery-template')
+  .content.querySelector('.gallery__item');
+
+//ДОМ элементы
+
 const openPopupButton = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
@@ -6,6 +43,9 @@ const nameInput = formElement.querySelector('.popup__name');
 const jobInput  = formElement.querySelector('.popup__job');
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
+const galleryConteiner = document.querySelector('.gallery__list');
+
+//открытие и закрытие формы
 
 function popupToggle() {
   if (!popup.classList.contains('popup_opened')) {
@@ -16,12 +56,38 @@ function popupToggle() {
   popup.classList.toggle('popup_opened');
 }
 
+//редактирование информации о себе
+
 function formSubmitHandler(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
   popupToggle();
 }
+
+//генерация карточки
+
+const generateGalleryCard = (galleryCard) => {
+  const newGalleryCard = galleryCardTemlate.cloneNode(true);
+
+  const titleGalleryCard = newGalleryCard.querySelector('.gallery__title');
+  titleGalleryCard.textContent = galleryCard.name;
+
+  const imageGalleryCard = newGalleryCard.querySelector('.gallery__image');
+  imageGalleryCard.setAttribute('style', `background-image:url(${galleryCard.link})`);
+
+  return newGalleryCard;
+}
+
+//рендер карточки
+
+const renderGalleryCard = (galleryCard)=>{
+  galleryConteiner.append(generateGalleryCard(galleryCard));
+}
+
+initialCards.forEach((galleryCard) => {
+  renderGalleryCard(galleryCard);
+});
 
 openPopupButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
