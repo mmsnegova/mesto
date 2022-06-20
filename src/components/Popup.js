c36a7f3490825286cc1f31592cd36ff80897cf61
Mary Snegova <Marya0@yandex.ru>
2022-06-20 18:34:50 +0300

    refactor: вынести buttonClose в конструктор класса Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,7 @@
 export default class Popup {
     constructor(popupSelector){
         this._popup=document.querySelector(popupSelector);
+        this._buttonClose = this._popup.querySelector('.popup__close') 
     }
 
     _handleEscClose(evt){
@@ -10,8 +11,7 @@ export default class Popup {
     }
 
     setEventListeners(){
-        const buttonClose =  this._popup.querySelector('.popup__close', )
-        buttonClose.addEventListener('click',this.close.bind(this));
+        this._buttonClose.addEventListener('click',this.close.bind(this));
         this._popup.addEventListener('click',(evt)=>{
             if(evt.target===evt.currentTarget){
                 this.close();
