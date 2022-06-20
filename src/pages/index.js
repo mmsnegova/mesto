36f2db6cffae449c33415c8c8744b2b884fba9ce
Mary Snegova <Marya0@yandex.ru>
2022-06-20 10:40:37 +0300

    refator: вынестит const vailidationConfig в utils/cinstans.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,14 +34,7 @@ const cardList = new Section({
 cardList.renderItems();
 
 
-const validationConfig = {
-  formSelector: '.popup__body',
-  inputElement: '.popup__input',
-  buttonElement: '.popup__save',
-  inactiveButtonClass: 'popup__save_inactive',
-  inputErrorClass: 'popup__input_error',
-  errorClass:'popup__input-error_active'
-}
+
 
 
 const formValidators = {};
---
+++ src/utils/constans.js
@@ -0,0 +1,8 @@
+const validationConfig = {
+    formSelector: '.popup__body',
+    inputElement: '.popup__input',
+    buttonElement: '.popup__save',
+    inactiveButtonClass: 'popup__save_inactive',
+    inputErrorClass: 'popup__input_error',
+    errorClass:'popup__input-error_active'
+  }
