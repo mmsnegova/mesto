fbaf26a889a87b785670f6f81a1e47f7cfd10d44
Mary Snegova <Marya0@yandex.ru>
2022-06-20 18:38:57 +0300

    fix: вынести this._handleEscClose.bind(this) в конструктор класса Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,8 @@
 export default class Popup {
     constructor(popupSelector){
         this._popup=document.querySelector(popupSelector);
-        this._buttonClose = this._popup.querySelector('.popup__close') 
+        this._buttonClose = this._popup.querySelector('.popup__close');
+        this._handleEscClose = this._handleEscClose.bind(this);
     }
 
     _handleEscClose(evt){
@@ -20,12 +21,12 @@ export default class Popup {
     }
     open(){
         this._popup.classList.add('popup_opened');
-        document.addEventListener('keydown',this._handleEscClose.bind(this));
+        document.addEventListener('keydown',this._handleEscClose);
     }
 
     close(){
         this._popup.classList.remove('popup_opened');
-        document.removeEventListener('keydown',this._handleEscClose.bind(this));
+        document.removeEventListener('keydown',this._handleEscClose);
     };
 }
 
